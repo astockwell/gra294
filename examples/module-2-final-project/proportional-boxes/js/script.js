1a3913aecabe943260704b16b0cc9b50ebdfa7ec
Alex Stockwell <astockwell@gmail.com>
2014-04-15 23:58:55 -0700

    added comments to proportional-boxes js

--- examples/module-2-final-project/proportional-boxes/js/script.js
+++ examples/module-2-final-project/proportional-boxes/js/script.js
@@ -1,5 +1,5 @@
 /*
- * Setup variables. Save the elements we want to use later (increases performance)
+ * Setup (cache) jQuery selectors. Learn about caching variables here: http://mikeadamsnc.com/cache-jquery-selectors-for-better-performance/
  */
 var $window = $(window);
 var $outer  = $('.outer');
@@ -9,10 +9,10 @@ var $middle = $('.middle');
  * Function that finds the width of the '.outer' div and sets it's height and it's (and '.middle' div's) padding proportionately
  */
 var reProportionBoxes = function() {
-	var width = $outer.outerWidth();
+	var width = $outer.outerWidth(); // Learn about jQuery's ".outerWidth()" method here: http://api.jquery.com/outerwidth/
 	var proportion = width / 5;
 
-	$outer.css('height', width);
+	$outer.css('height', width); // Learn about jQuery's ".css()" method here: http://api.jquery.com/css/
 
 	$outer.css('padding', proportion + "px");
 	$middle.css('padding', proportion + "px");
@@ -21,5 +21,5 @@ var reProportionBoxes = function() {
 /*
  * Run the function 'reProportionBoxes' when the page first loads and when it's resized
  */
-$window.on('load', reProportionBoxes);
+$window.on('load', reProportionBoxes); // Learn about jQuery's ".on()" method here: http://api.jquery.com/on/
 $window.on('resize', reProportionBoxes);
